0a39a3da99f850b77ed5c2068fe8a337cfb0bb45
Hpr-web <nikhilhpr18@gmail.com>
2023-10-25 16:27:25 +0530

    salting & hashing using bcrypt

--- app.js
+++ app.js
@@ -3,80 +3,87 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 
 app.use(express.static("public"));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 
+//this is due to dotenv 
 console.log(process.env.SECRET);
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+    await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 
-  const userSchema = new mongoose.Schema ({
-    email:String,
-    password:String
-  });
-
-  const user = mongoose.model("User", userSchema);
-
-  app.get("/", (req, res)=>{
-    res.render("home")
-});
-
-app.get("/login", (req, res)=>{
-    res.render("login")
-});
-
-app.get("/register", (req, res)=>{
-    res.render("register")
-});
-
-app.post("/register", async(req, res)=> {
-    const email = req.body.username; 
-    const password = md5(req.body.password); 
-
-    const newUser = new user({
-        email:email,
-        password:password
+    const userSchema = new mongoose.Schema({
+        email: String,
+        password: String
     });
 
-    await newUser.save();
+    const user = mongoose.model("User", userSchema);
 
-    res.render("secrets");
+    app.get("/", (req, res) => {
+        res.render("home")
+    });
 
-    console.log(email);
-    console.log(password);
-});
+    app.get("/login", (req, res) => {
+        res.render("login")
+    });
 
-app.post("/login", async(req, res) =>{
-    const username = req.body.username;
-    const password = md5(req.body.password);
+    app.get("/register", (req, res) => {
+        res.render("register")
+    });
 
-    console.log(username);
-    console.log(password);
+    app.post("/register", async (req, res) => {
+        const email = req.body.username;
+        const password = req.body.password;
+
+        console.log(email);
+        console.log(password);
+
+        bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+
+            const newUser = new user({
+                email: email,
+                password: hash
+            });
+                await newUser.save();
+                res.render("secrets");   
+        });
+
+    });
+
+    app.post("/login", async (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
     
-    const foundUser = await user.findOne({email:username});
-    console.log(foundUser);
-
-    if(foundUser){
-        if(foundUser.password === password){
-            res.render("secrets")
-        }else{
-            res.json("mess:Incorrect password")
+        console.log(username);
+        console.log(password);
+    
+        const foundUser = await user.findOne({ email: username });
+        console.log(foundUser);
+    
+        if (foundUser) {
+            bcrypt.compare(password, foundUser.password, async function (err, result) {
+                if (result === true) {
+                    res.render("secrets");
+                } else {
+                    res.json({ message: "Incorrect password" });
+                }
+            });
+        } else {
+            res.json({ message: "Incorrect username" });
         }
-    }else{
-        res.json("mess:Incorrect username")
-    }   
-});
+    });
+    
 
-app.listen(3000, ()=>{
-    console.log("server is listening on port:3000")
-});
+    app.listen(3000, () => {
+        console.log("server is listening on port:3000")
+    });
 
 }
 
